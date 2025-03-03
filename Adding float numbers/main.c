782142d385823939eaa4d20b2ec71b7840ccd7d4
GalibDev <mirza.galib.palash@gmail.com>
2025-03-03 22:55:21 +0600

    Add files via upload

---
+++ Adding float numbers/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <conio.h>
+main()
+{
+
+  float n1,n2,sum;
+  printf("Enter the first number");
+  scanf("%f", &n1);
+
+printf("Enter the second number");
+    scanf("%f", &n2);
+    sum =n1+n2;
+    printf("sum of two numbers = %f",sum);
+
+
+
+
+
+
+}
